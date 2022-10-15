a67f3519f9a0bb4a241e27162c3a2103c8200a2a
Kevin <57250095+armyant0920@users.noreply.github.com>
2022-10-16 01:51:26 +0800

    Add files via upload

---
+++ Serizlization_Example/Fish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serizlization_Example
+{
+    //[Serializable]
+    public class Fish
+    {
+
+        public string name { get; set; }
+        public string food { get; set; }
+        public string gender { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("fish:name {0},food {1},gender {2}", name, food, gender);
+        }
+    }
+}
---
+++ Serizlization_Example/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Serialization;
+
+namespace Serizlization_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Fish a = new Fish();
+            a.food = "meat";
+            a.name = "A_Fish";
+            a.gender = "male";
+
+            Fish b = new Fish();
+            b.food = "vegt";
+            b.name = "B_Fish";
+            b.gender = "female";
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            using (FileStream fs = new FileStream("TESTFile.xml", FileMode.OpenOrCreate)) { 
+            
+                DataContractSerializer dcs = new DataContractSerializer(typeof(Fish));
+                dcs.WriteObject(fs, a);
+            
+            }
+
+
+            using (FileStream fs = new FileStream("TESTFile.xml", FileMode.Open))
+            {
+
+                DataContractSerializer dcs = new DataContractSerializer(typeof(Fish));
+                Fish c = dcs.ReadObject(fs) as Fish;
+                Console.WriteLine("fish c:{0}", c);
+
+            }
+
+        }
+    }
+}
